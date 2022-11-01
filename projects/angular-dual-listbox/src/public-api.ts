5d44b424d491e69d96bab7d1d63951792ddf7271
TreZc0 <trezc0@gmx.de>
2022-11-01 22:53:03 +0100

    dist only for testing

--- projects/angular-dual-listbox/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-dual-listbox
- */
-
-export * from './lib/basic-list';
-export * from './lib/dual-list.component';
-export * from './lib/angular-dual-listbox.module';
